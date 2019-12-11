3bb346d2d97f297e4630d325d074dc8db45d3ae0
Arun Suresh <17378444+arunsuresh16@users.noreply.github.com>
2019-12-10 22:11:30 -0500

    Kalman filter (#17)
    Adding Extended Kalman Filter Project.
    Renaming Other Term1 projects.

---
+++ Self_Driving_Car_Engineer/Term1/CarND-Extended-Kalman-Filter/src/tools.cpp
@@ -0,0 +1,85 @@
+#include "tools.h"
+#include <iostream>
+
+using Eigen::VectorXd;
+using Eigen::MatrixXd;
+using std::vector;
+using std::cout;
+using std::endl;
+
+Tools::Tools() {}
+
+Tools::~Tools() {}
+
+VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
+                              const vector<VectorXd> &ground_truth) {
+  /**
+   * Calculate the RMSE here.
+   */
+  VectorXd rmse(4);
+  rmse = VectorXd::Zero(4);
+
+  // check the validity of the inputs:
+  if ((estimations.size() != ground_truth.size())
+      || (estimations.empty()) || (ground_truth.empty())) {
+    cout << "CalculateRMSE: Invalid estimation or ground_truth data" << endl;
+    return rmse;
+  }
+
+  // accumulate squared residuals
+  for (unsigned int i=0; i < estimations.size(); ++i) {
+
+    VectorXd residual = estimations[i] - ground_truth[i];
+
+    // coefficient-wise multiplication
+    residual = residual.array()*residual.array();
+    rmse += residual;
+  }
+
+  // calculate the mean
+  rmse = rmse/estimations.size();
+
+  // calculate the squared root
+  rmse = rmse.array().sqrt();
+
+  // return the result
+  return rmse;
+}
+
+MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
+  /**
+   * Calculate a Jacobian Matrix.
+   */
+  MatrixXd Hj(3,4);
+  // Initialize Hj to return 0s in case of error
+  Hj = MatrixXd::Zero(3, 4);
+  if(x_state.size() != 4) {
+    cout << "CalculateJacobian: x_state size is invalid " << x_state.size() << endl;
+    return Hj;
+  }
+
+  // recover state parameters
+  float px = x_state(0);
+  float py = x_state(1);
+  float vx = x_state(2);
+  float vy = x_state(3);
+
+  // pre-compute a set of terms to avoid repeated calculation
+  float c1 = px*px+py*py;
+  float c2 = sqrt(c1);
+  float c3 = (c1*c2);
+
+  // check division by zero
+  if (fabs(c1) < 0.0001) {
+    cout << "CalculateJacobian () - Error - Division by Zero" << endl;
+    Hj.fill(0.0);
+    return Hj;
+  }
+
+  // compute the Jacobian matrix
+  Hj << (px/c2), (py/c2), 0, 0,
+      -(py/c1), (px/c1), 0, 0,
+      py*(vx*py - vy*px)/c3, px*(px*vy - py*vx)/c3, px/c2, py/c2;
+
+  return Hj;
+}
